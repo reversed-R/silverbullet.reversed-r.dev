ad2b62a4f29cac056258ad25cdbf375cdceca747
reversed-R <qiu1287shan@gmail.com>
2025-12-08 19:45:28 +0900

    feat: add mdbook to publish

---
+++ mdbook/entrypoint.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+find src -name '*.md' | sort > tmp
+echo "# Summary" > src/SUMMARY.md
+
+while read f; do
+  # remove leading "src/"
+  rel="${f#src/}"
+
+  name=$(basename "$rel" .md)
+  echo "* [$name]($rel)" >> src/SUMMARY.md
+done < tmp
+
+rm tmp
+
+mdbook serve -n 0.0.0.0 -p 80
